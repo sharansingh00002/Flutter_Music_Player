71edfb29b45da7e8d5a7369830d80772a7585e3c
Sharanjyot SIngh <sharansingh00002@gmail.com>
2019-02-17 21:48:04 +0530

    minor changes

--- lib/Home/bloc/home-page-bloc.dart
+++ lib/Home/bloc/home-page-bloc.dart
@@ -2,12 +2,9 @@ import 'package:rxdart/rxdart.dart';
 
 class HomeBlocProvider {
   static HomeBlocProvider bloc = HomeBlocProvider();
-  final _songBloc = BehaviorSubject();
   final _songColorBloc = BehaviorSubject();
 
-  Function get changeSong => _songBloc.sink.add;
   Function get changeSongColorDetails => _songColorBloc.sink.add;
 
-  Stream get songStream => _songBloc.stream;
   Stream get songColorStream => _songColorBloc.stream;
 }
